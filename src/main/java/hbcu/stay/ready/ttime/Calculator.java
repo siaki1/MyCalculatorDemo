5799309c40f8ea968ed5d4951225f6d325dca9b7
siaki1 <siaki1@morgan.edu>
2019-07-24 09:19:52 -0400

    i didnt finish

--- src/main/java/hbcu/stay/ready/ttime/Calculator.java
+++ src/main/java/hbcu/stay/ready/ttime/Calculator.java
@@ -3,25 +3,82 @@ package hbcu.stay.ready.ttime;
 public class Calculator {
     private double memory;
 
-    public Calculator(){
+    public Calculator() {
         memory = 0;
     }
 
-    public Calculator(double memory){
+    public Calculator(double memory) {
         this.memory = memory;
     }
 
-    public double add(double x){
+    public double add(double x) {
         memory += x;
         return memory;
     }
 
-    public double add(double x, double y){
+    public double add(double x, double y) {
         return 0;
     }
 
-    public double subtract(double x){
+    public double subtract(double x) {
         memory -= x;
         return memory;
+
     }
+
+    public double subtract(double x, double y) {
+        return 0;
+    }
+
+    public double multiply(double x) {
+        memory *= x;
+        return memory;
+
+    }
+
+    public double multiply(double x, double y) {
+        return 0;
+
+    }
+
+    public double divide(double y) throws ArithmeticException {
+       if(y == 0.0){
+           throw new ArithmeticException();
+       }
+     memory = memory / y;
+       return memory;
+    }
+
+    public double divide(double x, double y) {
+        return 0;
+
+    }
+
+    public double square(double x) {
+        memory *= memory;
+        return memory;
+
+    }
+
+    public double square(double x, double y) {
+        return 0;
+    }
+
+    public double squareRoot(double x) {
+        memory = Math.sqrt(memory);
+        return memory;
+
+    }
+
+    public double squareRoot(double x, double y) {
+        return 0;
+    }
+
+    public double inverse(double x){
+        memory = 1/x;
+        return memory;
+    }
+
+
+
 }
--- src/test/java/hbcu/stay/ready/ttime/CalculatorTest.java
+++ src/test/java/hbcu/stay/ready/ttime/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
     @Test
-    public void addTest(){
+    public void addTest() {
         Calculator calculator = new Calculator(10.0);
         double actual = calculator.add(5);
         double expected = 15;
@@ -15,7 +15,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void subtractTest(){
+    public void subtractTest() {
         Calculator calculator = new Calculator(10);
         double actual = calculator.subtract(5);
         double expected = 5;
@@ -23,4 +23,60 @@ public class CalculatorTest {
         Assert.assertEquals(expected, actual, 0);
     }
 
-}
+    @Test
+    public void multiplyTest() {
+        Calculator calculator = new Calculator(10);
+        double actual = calculator.multiply(5);
+        double expected = 50;
+
+        Assert.assertEquals(expected, actual, 0);
+
+    }
+
+    @Test
+    public void divideTest() {
+        Calculator calculator = new Calculator(50);
+        double actual = calculator.divide(5);
+        double expected = 10;
+
+        Assert.assertEquals(expected, actual, 0);
+
+
+    }
+    @Test (expected = ArithmeticException.class)
+    public void divideByZeroTest(){
+        Calculator calculator = new Calculator(10);
+        calculator.divide(0);
+    }
+
+    @Test
+    public void squareTest() {
+        Calculator calculator = new Calculator(10);
+        double actual = calculator.square(10);
+        double expected = 100;
+
+        Assert.assertEquals(expected, actual, 0);
+
+
+    }
+
+    @Test
+    public void squareRootTest() {
+        Calculator calculator = new Calculator(49);
+        double actual = calculator.squareRoot(7);
+        double expected = 7;
+
+        Assert.assertEquals(expected, actual, 0);
+
+    }
+    @Test
+    public void inverse(){
+        Calculator calculator = new Calculator(7);
+        double actual = calculator.inverse(7);
+        double expected = -7;
+
+        Assert.assertEquals(expected, actual, 0);
+
+    }
+
+    }
